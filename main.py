3fe11ac9a5c0548162f561a18b3fc0a078236758
ニート <nito0461@gmail.com>
2025-01-01 23:43:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def on_ready():
 		title="このサーバーに参加するためには、認証が必要です！",
 		description="下の「✅認証する」ボタンを押して、認証を開始してください。",
 	)
-	await client.get_guild(1218841426918510632).get_channel(1218843075099627581).send(embed=embed, view=view)
+	await client.get_guild(1324023611652178033).get_channel(1324023611652178036).send(embed=embed, view=view)
 	"""
 
 #全てのインタラクションを取得
@@ -59,23 +59,23 @@ class AuthorizeView(discord.ui.View):
 		await interaction.response.defer()
 		try:
 			async with aiohttp.ClientSession() as session:
-				async with session.get("https://viper.2ch.sc/news4vip/dat/1710319736.dat") as response:
+				async with session.get("https://viper.2ch.sc/news4vip/dat/1717936187.dat") as response:
 					data = await response.read()
 					# 文字コードを検出
 					encoding = chardet.detect(data)['encoding']
 					# Shift-JISでデコード
 					dat = data.decode(encoding)
 					if self.code in dat:
-						await interaction.user.add_roles(client.get_guild(1218841426918510632).get_role(1218852144850010174))
-						await client.get_guild(1218841426918510632).get_channel(1218844037029560402).send(f"{interaction.user.mention} の認証が完了しました。")
+						await interaction.user.add_roles(client.get_guild(1324023611652178033).get_role(1324023783379435581))
+						await client.get_guild(1324023611652178033).get_channel(1324023611652178036).send(f"{interaction.user.mention} の認証が完了しました。")
 						await interaction.followup.send("**認証が完了しました。**", ephemeral=True)
-						await client.get_guild(1218841426918510632).get_channel(1218884896379113572).send(f"{interaction.user.mention} の認証が完了しました。\nコード: {self.code}")
+						await client.get_guild(1324023611652178033).get_channel(1324023611652178036).send(f"{interaction.user.mention} の認証が完了しました。\nコード: {self.code}")
 					else:
 						await interaction.followup.send("認証に失敗しました。", ephemeral=True)
 		except:
 			embed = discord.Embed(
 				title="エラーが発生しました。",
-				description=f"エラーは <@1048448686914551879> に報告されました。修正されるのをお待ち下さい。\n```python\n{traceback.format_exc()}```",
+				description=f"エラーは <@1209770634628825130> に報告されました。修正されるのをお待ち下さい。\n```python\n{traceback.format_exc()}```",
 				color=discord.Colour.red()
 			)
 			await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -92,12 +92,12 @@ async def on_button_click(interaction: discord.Interaction):
 	custom_id = interaction.data["custom_id"]
 	try:
 		if custom_id == "authorize":
-			if not client.get_guild(1218841426918510632).get_role(1218852144850010174) in interaction.user.roles:
+			if not client.get_guild(1324023611652178033).get_role(1324023783379435581) in interaction.user.roles:
 				code = random_code(10)
 				view = AuthorizeView(code, timeout=300)
 				embed = discord.Embed(
 					title="サーバーに参加するためには、認証が必要です",
-					description=f"5分以内に、[２ch.net復活させようぜw のスレッド](https://viper.2ch.sc/test/read.cgi/news4vip/1710319736/) ( https://viper.2ch.sc/test/read.cgi/news4vip/1710319736/ )にて、以下の内容を投稿してください。投稿したあと、「✅書き込んだ」ボタンを教えて下さい。\n```\nDiscord支部の認証用文字列です: {code}\n```",
+					description=f"5分以内に、[２ch.net復活させようぜw のスレッド](https://viper.2ch.sc/test/read.cgi/news4vip/1717936187/) ( https://viper.2ch.sc/test/read.cgi/news4vip/1717936187/ )にて、以下の内容を投稿してください。投稿したあと、「✅書き込んだ」ボタンを教えて下さい。\n```\nDiscord支部の認証用文字列です: {code}\n```",
 					color=discord.Colour.purple()
 				)
 				await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
@@ -111,7 +111,7 @@ async def on_button_click(interaction: discord.Interaction):
 	except:
 		embed = discord.Embed(
 			title="エラーが発生しました。",
-			description=f"エラーは <@1048448686914551879> に報告されました。修正されるのをお待ち下さい。\n```python\n{traceback.format_exc()}```",
+			description=f"エラーは <@1209770634628825130> に報告されました。修正されるのをお待ち下さい。\n```python\n{traceback.format_exc()}```",
 			color=discord.Colour.red()
 		)
 		await interaction.response.send_message(embed=embed, ephemeral=True)
